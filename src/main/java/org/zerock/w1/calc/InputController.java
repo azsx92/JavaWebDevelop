81018aa4fc8f92249980847afdefb101e7413bd8
azsx92 <azsx92@gmail.com>
2023-07-05 19:02:26 +0900

    p53 end

--- src/main/java/org/zerock/w1/calc/InputController.java
+++ src/main/java/org/zerock/w1/calc/InputController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class InputController extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req , HttpServletResponse resp) throws ServletException, IOException {
 
         System.out.println("InputController...doGet...");
 
